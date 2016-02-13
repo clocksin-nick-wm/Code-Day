d30c1e3e2dd97a16e5717f5da8fe9f054a7e9fd5
nicolas clocksin <nac1998@cox.net>
2016-02-13 14:00:29 -0700

    Start up on navigation bar and also added js charts.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+    <html>
+<head>
+    <title>CodeDay</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">CodeDay</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
+
+<script type="text/javascript"  src="jquery.js"></script>
+</body>
+</html>
