7ce2047e053b04a56dea7d046b12843fe1545ecf
nicolas clocksin <nac1998@cox.net>
2016-02-13 16:39:07 -0700

    no message

--- user-usage.php
+++ user-usage.php
@@ -3,14 +3,28 @@
 <header>
     <title>User Usage</title>
     <?php
-    include('header.php')
+    include('header.php');
+    include('connect.php');
     ?>
 </header>
 <body>
 <?php
 include('navbar.php')
 ?>
-
+<table>
+    <?php
+    $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    $results = $mysqli->query("SELECT username, userRewards FROM users ORDER BY id ASC");
+    if($results) {
+    $user_rewards = '<ul class="products">';
+    //fetch results set as object and output HTML
+    while ($obj = $results->fetch_object()) {
+    ?>
+    ?>
+    <tbody>
+    <tr></tr>
+    </tbody>
+</table>
 </body>
 
 </html>
