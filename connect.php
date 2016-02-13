6b992ce210240ee7ff5a9f5bccfd554fcf728bc9
nicolas clocksin <nac1998@cox.net>
2016-02-13 15:26:08 -0700

    Added Chart.js from Charles design. Also created a connect php.

--- connect.php
+++ connect.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: visitor
- * Date: 2/13/2016
- * Time: 2:13 PM
- */
+$currency = '&#8377; '; //Currency Character or code
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'CodeDay';
+$db_host = 'localhost';
+
+$shipping_cost      = 1.50; //shipping cost
+$taxes              = array( //List your Taxes percent here.
+    'VAT' => 12,
+    'Service Tax' => 5
+);
+//connect to MySql
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+?>
--- index.php
+++ index.php
@@ -9,23 +9,91 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="Chart.js-master/Chart.js"></script>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
 <?php
 include ('navbar.php');
 ?>
 
-<canvas id="myChart" width="400" height="400">
-    var myLineChart = new Chart(ctx).Line(data, options);
+<canvas id="myChart" width="400" height="400"></canvas>
 
-</canvas>
+<button id="changeValues">Change</button>
+<script>
 
-<script src="Chart.js-master/Chart.js">
+
+
+    var twitter = 34221,
+        facebook = 55219,
+        googleP = 111213,
+        total = twitter + facebook + googleP,
+        twoTwitter = 24,
+        twoFacebook = 12,
+        twoGoogleP = 6,
+        twoTotal = twoTwitter + twoFacebook + twoGoogleP,
+        currentDataSet = 0;
+
+
+    var data = [{
+        value: twitter,
+        color: "#00aced",
+        label: "Twitter (" + Math.round((twitter / total) * 100) + "%)"
+    }, {
+        value: facebook,
+        color: "#3b5998",
+        label: "Facebook (" + Math.round((facebook / total) * 100) + "%)"
+    }, {
+        value: googleP,
+        color: "#dd4b39",
+        label: "Google Plus (" + Math.round((googleP / total) * 100) + "%)"
+    }
+
+    ];
+
+    var options = {
+        animation: true,
+        animationEasing: "easeOutBounce",
+        segmentStrokeWidth: 0
+    };
+
+    //Get the context of the canvas element we want to select
+    var c = $('#myChart');
+    var ct = c.get(0).getContext('2d');
     var ctx = document.getElementById("myChart").getContext("2d");
-    var myNewChart = new Chart(ctx).PolarArea(data);
-    new Chart(ctx).PolarArea(data, options);
+    /*************************************************************************/
+    chart = new Chart(ct).Doughnut(data, options);
 
+
+    $("#changeValues").click(function() {
+
+
+        if (currentDataSet == 0) {
+            chart.segments[0].value = twoTwitter;
+            chart.segments[1].value = twoFacebook;
+            chart.segments[2].value = twoGoogleP;
+
+            chart.segments[0].label = "Twitter (" + Math.round((twoTwitter / twoTotal) * 100) + "%)";
+            chart.segments[1].label = "Facebook (" + Math.round((twoFacebook / twoTotal) * 100) + "%)";
+            chart.segments[2].label = "Google Plus (" + Math.round((twoGoogleP / twoTotal) * 100) + "%)";
+            currentDataSet = 1;
+        } else {
+            chart.segments[0].value = twitter;
+            chart.segments[1].value = facebook;
+            chart.segments[2].value = googleP;
+
+            chart.segments[0].label = "Twitter (" + Math.round((twitter / total) * 100) + "%)";
+            chart.segments[1].label = "Facebook (" + Math.round((facebook / total) * 100) + "%)";
+            chart.segments[2].label = "Google Plus (" + Math.round((googleP / total) * 100) + "%)";
+            currentDataSet = 0;
+        }
+
+        chart.update();
+
+
+    });
 </script>
+
 <script type="text/javascript"  src="jquery.js"></script>
 </body>
 </html>
