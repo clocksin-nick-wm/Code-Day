a754be4c37a49d9b62109bafdb2ce74ec74cc4b5
Charles Rogers <billyrogersiv@gmail.com>
2016-02-13 15:31:05 -0700

    no message

--- connect.php
+++ connect.php
@@ -5,7 +5,6 @@ $db_password = '';
 $db_name = 'CodeDay';
 $db_host = 'localhost';
 
-);
 //connect to MySql
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 if ($mysqli->connect_error) {
