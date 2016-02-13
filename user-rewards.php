eedc771324de22f3731c5d5f7a084038b09dc414
Kyle Chapman <xeuorux@gmail.com>
2016-02-13 16:05:34 -0700

    Creates user rewards page.

---
+++ user-rewards.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<header>
+    <title>User Usage</title>
+    <?php
+    include('header.php')
+    ?>
+</header>
+<body>
+<?php
+include('navbar.php')
+?>
+
+</body>
+
+</html>
