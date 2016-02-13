4eaa22d7963a1a2bd55bfefdea9de953a921986f
nicolas clocksin <nac1998@cox.net>
2016-02-13 15:27:36 -0700

    Edited connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,10 @@
 <?php
-$currency = '&#8377; '; //Currency Character or code
 
 $db_username = 'root';
 $db_password = '';
 $db_name = 'CodeDay';
 $db_host = 'localhost';
 
-$shipping_cost      = 1.50; //shipping cost
-$taxes              = array( //List your Taxes percent here.
-    'VAT' => 12,
-    'Service Tax' => 5
 );
 //connect to MySql
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
