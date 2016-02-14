302deb2ae9c0370226f9cb03409c768a96b5f72f
Charles Rogers <billyrogersiv@gmail.com>
2016-02-14 00:18:02 -0700

    test

--- scripts-mobile.js
+++ scripts-mobile.js
@@ -4,11 +4,11 @@ jQuery('document').ready(function(){
     $('#button1').on('click', function (){
         $('#button1').css({display: "none"});
         $('#button2').css({display: "inherit"});
-        $("#list-table").slideToggle();
+        $("#list-table").toggle();
     });
     $('#button2').on('click', function (){
         $('#button1').css({display: "inherit"});
         $('#button2').css({display: "none"});
-        $("#list-table").slideToggle();
+        $("#list-table").toggle();
     });
 });
