9a849bbb55d5d5be33a10c9e2f6944fd70b5317a
Charles Rogers <billyrogersiv@gmail.com>
2016-02-14 02:28:19 -0700

    please for the love of fucking god

--- current-rewards.php
+++ current-rewards.php
@@ -4,16 +4,24 @@ require_once('connect.php');
 session_start();
 
 $query = "SELECT * FROM rewards INNER JOIN client ON client.id = rewards.client_id WHERE client_id = {$_SESSION['user_id']}";
-$delete = "DELETE * FROM rewards WHERE client_id = {$_SESSION['user_id']}";
+
+if(@$_POST['removeButton'])
+{
+    $reward_to_del = $_POST['reward_to_delete'];
+    $delete_query = "DELETE * FROM rewards WHERE reward_name=$reward_to_del";
+    $delete_query = mysqli(mysqli, $delete_query);
+    if(! $delete_query)
+        die(mysqli_error($mysqli));
+}
+
+
 $result = mysqli_query($mysqli, $query);
 $remove = mysqli_query($mysqli, $delete);
 
 if(! $result) {
-    die(mysqli_error($mysqli));
+
 }
-if(! $remove){
-    die(mysqli_error($mysqli));
-}
+
 ?>
 <!DOCTYPE html>
     <html>
@@ -48,7 +56,7 @@ include('navbar.php');
                 echo "<td>$rewardname</td>";
                 echo "<td>$valuepoints</td>";
                 echo "<td>$rewarddescription</td>";
-                echo "<td><button class='alert alert-danger'>e</button></td>";
+                echo "<td><form method='post'><input type='hidden' name='reward_to_delete' value='$rewardname' /><button class='alert alert-danger' type='submit' name='removeButton'>Remove</button></td>";
                 echo '</tr>';
 
                     }
