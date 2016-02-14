81811efc2c6403b983353c9238e16e752a1272bf
Charles Rogers <billyrogersiv@gmail.com>
2016-02-14 03:06:56 -0700

    Title

--- barcode_reader.php
+++ barcode_reader.php
@@ -10,6 +10,7 @@
     <script src="java.js" type="text/javascript"></script>
 </head>
 <body style="background-color: white;">
+    <h3 id="fs-subtitle" style="font-family: 'Pacifico', cursive; color: #36abcf;" >Points</h3>
     <a href="zxing://scan/?ret=<?php echo urlencode("http://10.204.1.9/CodeDay/returnBarcode.php?code={CODE}"); ?>&SCAN_FORMATS=UPC_A,EAN_13,QR_CODE" class="scan">
         <input type="submit" name="formSubmit" value="Submit" class="btn" style="width: 174px; text-align: center; height:45px;"/>
     </a>
