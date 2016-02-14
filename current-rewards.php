6280cab2ad6c5e08d14a0965b513bc0df4f506b7
nicolas clocksin <nac1998@cox.net>
2016-02-14 02:20:22 -0700

    no message

--- current-rewards.php
+++ current-rewards.php
@@ -4,9 +4,14 @@ require_once('connect.php');
 session_start();
 
 $query = "SELECT * FROM rewards INNER JOIN client ON client.id = rewards.client_id WHERE client_id = {$_SESSION['user_id']}";
+$delete = "DELETE * FROM rewards WHERE client_id = {$_SESSION['user_id']}";
 $result = mysqli_query($mysqli, $query);
+$remove = mysqli_query($mysqli, $delete);
 
 if(! $result) {
+    die(mysqli_error($mysqli));
+}
+if(! $remove){
     die(mysqli_error($mysqli));
 }
 ?>
@@ -35,12 +40,19 @@ include('navbar.php');
                 $rewardname = $rewards['reward_name'];
                 $valuepoints = $rewards['point_value'];
                 $rewarddescription = $rewards['description'];
+                if (mysqli_num_rows($remove)>0){
+                    while ($remove = mysqli_fetch_assoc($delete)) {
+                        $cancel = $delete['reward_name'];
 
                 echo '<tr>';
                 echo "<td>$rewardname</td>";
                 echo "<td>$valuepoints</td>";
                 echo "<td>$rewarddescription</td>";
+                echo "<td><button class='alert alert-danger'>e</button></td>";
                 echo '</tr>';
+
+                    }
+                }
             }
         } else {
             echo "0 results";
