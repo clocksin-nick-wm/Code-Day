271a8202fac184db049c662aeff888964405af5f
nicolas clocksin <nac1998@cox.net>
2016-02-14 03:06:34 -0700

    no message

--- navbar.php
+++ navbar.php
@@ -1,21 +1,19 @@
 <link rel="stylesheet" type="text/css" href="main.css">
-<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
             <a class="navbar-brand" href="index.php" style="font-family: 'Pacifico'; color: #3CD0E9">Points</a>
         </div>
         <div id="nav">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="user-usage.php">User Usage</a></li>
-            <li><a href="user-rewards.php">User Rewards</a></li>
-            <li><a href="rewards-input.php">Rewards Input</a> </li>
-            <li><a href="current-rewards.php">Current Rewards</a> </li>
-            <li><a href="setting.php">Settings</a></li>
-            <li><a href="logout.php" id="logout">Logout</a></li>
-        </ul>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="user-usage.php">User Usage</a></li>
+                <li><a href="user-rewards.php">User Rewards</a></li>
+                <li><a href="rewards-input.php">Rewards Input</a> </li>
+                <li><a href="current-rewards.php">Current Rewards</a> </li>
+                <li><a href="setting.php">Settings</a></li>
+                <li><a href="logout.php" id="logout">Logout</a></li>
+            </ul>
         </div>
     </div>
 </nav>
